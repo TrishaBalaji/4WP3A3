fe8ed9188adb0507a1b5945204c6de92a5cb60f5
TrishaBalaji <31964190+TrishaBalaji@users.noreply.github.com>
2026-03-28 07:58:25 -0400

    Error opening file from folder in editors.js

--- editors.js
+++ editors.js
@@ -1,6 +1,6 @@
 const express = require('express');
 var router = express.Router()
-const editor = require('.app.js');
+const editor = require('.app.js'); //error opening file from folder 
 
 // Display the editors page
 router.get("/", editor, async function(req, res)
